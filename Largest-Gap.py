0dd38eed58a37fa55c35492cd8f2a2057780752f
Sadriddin <Sadriddindev@gmail.com>
2022-04-16 23:14:31 +0500

    Moved to Problems folder

--- Largest-Gap.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def solve(self, nums):
-        nums.sort()
-        d = 0
-        for i in range(len(nums)-1):
-            if nums[i+1] - nums[i] > d:
-                d = nums[i+1]-nums[i]
-        return d
--- Narcissistic-Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def solve(self, n):
-        n1 = n
-        l = len(str(n))
-        s = 0
-        while n:
-            s += (n%10)**l
-            n//=10
-        return n1==s 
